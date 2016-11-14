d18a2d9e7278a8cbac6eb615fe1f019fc67976d6
Emilio Peláez <i.am@emiliopelaez.me>
2016-11-14 00:32:55 -0600

    CGFloat, Float and Double tests

--- Example/Tests/CGFloatTests.swift
+++ Example/Tests/CGFloatTests.swift
@@ -11,4 +11,33 @@ import Nimble
 import CGMathSwift
 
 class CGFloatSpec: QuickSpec {
+	override func spec() {
+		describe("CGFloat") {
+			let start: CGFloat = 5
+			let end: CGFloat = 10
+			let middle: CGFloat = (start + end) / 2
+			
+			let progressStart: CGFloat = 0
+			let progressEnd: CGFloat = 1
+			let progressMiddle: CGFloat = (progressStart + progressEnd) / 2
+			
+			it("lerp") {
+				expect(lerp(min: start, max: end, progress: progressStart)) == start
+				expect(lerp(min: start, max: end, progress: progressMiddle)) == middle
+				expect(lerp(min: start, max: end, progress: progressEnd)) == end
+			}
+			
+			it("inverseLerp") {
+				expect(inverseLerp(min: start, max: end, value: start)) == progressStart
+				expect(inverseLerp(min: start, max: end, value: middle)) == progressMiddle
+				expect(inverseLerp(min: start, max: end, value: end)) == progressEnd
+			}
+			
+			it("remap") {
+				expect(start.remap(fromRange: (start, end), toRange: (progressStart, progressEnd))) == progressStart
+				expect(middle.remap(fromRange: (start, end), toRange: (progressStart, progressEnd))) == progressMiddle
+				expect(end.remap(fromRange: (start, end), toRange: (progressStart, progressEnd))) == progressEnd
+			}
+		}
+	}
 }
--- Example/Tests/DoubleTests.swift
+++ Example/Tests/DoubleTests.swift
@@ -11,4 +11,33 @@ import Nimble
 import CGMathSwift
 
 class DoubleSpec: QuickSpec {
+	override func spec() {
+		describe("Double") {
+			let start: Double = 5
+			let end: Double = 10
+			let middle: Double = (start + end) / 2
+			
+			let progressStart: Double = 0
+			let progressEnd: Double = 1
+			let progressMiddle: Double = (progressStart + progressEnd) / 2
+			
+			it("lerp") {
+				expect(lerp(min: start, max: end, progress: progressStart)) == start
+				expect(lerp(min: start, max: end, progress: progressMiddle)) == middle
+				expect(lerp(min: start, max: end, progress: progressEnd)) == end
+			}
+			
+			it("inverseLerp") {
+				expect(inverseLerp(min: start, max: end, value: start)) == progressStart
+				expect(inverseLerp(min: start, max: end, value: middle)) == progressMiddle
+				expect(inverseLerp(min: start, max: end, value: end)) == progressEnd
+			}
+			
+			it("remap") {
+				expect(start.remap(fromRange: (start, end), toRange: (progressStart, progressEnd))) == progressStart
+				expect(middle.remap(fromRange: (start, end), toRange: (progressStart, progressEnd))) == progressMiddle
+				expect(end.remap(fromRange: (start, end), toRange: (progressStart, progressEnd))) == progressEnd
+			}
+		}
+	}
 }
